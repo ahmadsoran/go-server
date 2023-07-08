2345264d1082fe22fc87e925a3d088261796e4d9
ahmadsoran <ahmadsorann@gmail.com>
2023-07-08 19:36:41 +0300

    set mode

--- conf/EnvLoder.go
+++ conf/EnvLoder.go
@@ -3,10 +3,13 @@ package conf
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func EnvLoader() {
+	// if production use this
+	gin.SetMode(gin.DebugMode)
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
