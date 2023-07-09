076fa1aad96854afd8d7dc298e0f880595932b92
ahmadsoran <ahmadsorann@gmail.com>
2023-07-09 15:50:17 +0300

    remove debug

--- conf/EnvLoder.go
+++ conf/EnvLoder.go
@@ -3,13 +3,11 @@ package conf
 import (
 	"log"
 
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func EnvLoader() {
 	// if production use this
-	gin.SetMode(gin.DebugMode)
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
