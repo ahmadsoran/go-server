34b40ef76d1e88abd653f60e68cb7fa596547a80
ahmadsoran <ahmadsorann@gmail.com>
2023-07-09 09:57:15 +0300

    add logger & default model

--- conf/DB.go
+++ conf/DB.go
@@ -1,20 +1,35 @@
 package conf
 
 import (
+	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,        // Don't include params in the SQL log
+			Colorful:                  true,        // Disable color
+		},
+	)
 	// Connect to Db
 	var err error
 	dsn := os.Getenv("DSN")
 	dbName := os.Getenv("DB_NAME")
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 
 	if err != nil {
 		panic("Failed to connect to database")
